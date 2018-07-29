6ab64b7d56c54b64634c45abd427cc9ad2e460e9
bitu8759 <bitunirala.10@gmail.com>
2018-07-29 21:46:40 +0530

    -------Token Based OAuth set-up

---
+++ TokenBasedOAuthPOC/TokenBasedOAuthPOC/OwinOuth/CustomOAuthProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+
+
+namespace TokenBasedOAuthPOC.OwinOAuth
+{
+    public class CustomOAuthProvider: OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            if(context.UserName.ToLower() == "bnirala" && context.Password == "Nirala")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Bitu Kumar Nirala"));
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", context.UserName));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("Invalid  Grant", "User Name or Password is invalid");
+                return;
+            }
+        }
+
+
+    }
+}
---
+++ TokenBasedOAuthPOC/TokenBasedOAuthPOC/OwinOuth/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly: OwinStartup(typeof(TokenBasedOAuthPOC.OwinOAuth.Startup))]
+
+namespace TokenBasedOAuthPOC.OwinOAuth
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            // Set Global.asax WebApiConfig registration
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            // Aloow Cors
+            app.UseCors(CorsOptions.AllowAll);
+
+            // Create instance of Custom OAuth Provider
+            CustomOAuthProvider provider = new CustomOAuthProvider();
+
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions()
+            {
+                TokenEndpointPath = new PathString("/oauth/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(1),
+                AllowInsecureHttp = true,
+                Provider = provider
+            };
+
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            // Configure Http
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+
+
+        }
+    }
+}
